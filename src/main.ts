4ab09cb7e0d9e6ed73f1bd93e70e6ad5a964f243
VlaDinter <vladyslavdinter@gmail.com>
2024-03-22 18:53:44 +0200

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,6 +8,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Products example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(PORT);
 }
